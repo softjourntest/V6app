78ec9fea22fc768590fd2e09781a8a41c20121dc
PowWow Admin <admin@studio.dev.powwowinc.net>
2018-02-26 13:24:04 +0000

    [Studio] Update client version

--- client/src/app/app.hooks.ts
+++ client/src/app/app.hooks.ts
@@ -2,39 +2,19 @@ import { Injectable } from '@angular/core';
 import { AppHooks } from 'smartux-client';
 
 @Injectable()
-export class Hooks extends AppHooks {
-  /**
-   * Initial parameters to send to the server.
-   */
-  async getServerInitParams(): Promise<any> {
-    return {}
+export class Hooks implements AppHooks {
+  constructor() {
   }
 
-  /**
-   * Initialize the UI with data from the server.
-   */
-  initializeUI(params: any): void {
+  getServerInitParams() {
+    return Promise.resolve({});
   }
 
-  /**
-   * Override what happens when going to a new screen.
-   */
-  overrideStateHandler(oldScreen: string, newScreen: string, data: any): string {
+  initializeUI(params) {
+  }
+
+  overrideStateHandler(oldScreen, newScreen, data) {
     return newScreen;
-  }
-
-/**
-   * Override what happens when a custom URL scheme is called.
-   *
-   * type - 'event' is currently the only supported type.
-   * name - Name of event, e.g. 'login.submit'
-   * data - JSON object containing URL data.
-   *
-   * Returns: true - Continue with normal flow, e.g. if type is event, send the event to the server.
-   *          false - Don't continue with the normal flow.
-   */
-  async interceptCustomURLScheme(type: string, name: string, data: any): Promise<boolean> {
-    return true;
   }
 
 }
